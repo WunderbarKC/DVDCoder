150d6f0c8cef47cb5e26a9cebb6abdcc05a2e882
WunderbarKC <64834609+WunderbarKC@users.noreply.github.com>
2025-08-12 04:07:28 +0800

    Add files via upload

---
+++ promptings/PromptingFactory.py
@@ -0,0 +1,36 @@
+from promptings.CoT import CoTStrategy
+from promptings.Direct import DirectStrategy
+from promptings.Analogical import AnalogicalStrategy
+from promptings.SelfPlanning import SelfPlanningStrategy
+
+from promptings.MapCoder import MapCoder as MapCoder
+from promptings.Debate import Debate
+from promptings.Debate_r3 import Debate_R3
+from promptings.Debate_r2 import Debate_R2
+from promptings.Semantic import Semantic
+from promptings.SemanticOnly import SemanticOnly
+class PromptingFactory:
+    @staticmethod
+    def get_prompting_class(prompting_name):
+        if prompting_name == "CoT":
+            return CoTStrategy
+        elif prompting_name == "MapCoder":
+            return MapCoder
+        elif prompting_name == "Direct":
+            return DirectStrategy
+        elif prompting_name == "Analogical":
+            return AnalogicalStrategy
+        elif prompting_name == "SelfPlanning":
+            return SelfPlanningStrategy
+        elif prompting_name == "Debate":
+            return Debate
+        elif prompting_name == "Debate-r3":
+            return Debate_R3
+        elif prompting_name == "Debate-r2":
+            return Debate_R2
+        elif prompting_name == "Semantic":
+            return Semantic
+        elif prompting_name == "SemanticOnly":
+            return SemanticOnly
+        else:
+            raise Exception(f"Unknown prompting name {prompting_name}")
